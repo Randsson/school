11df86fa43f08ec288cba96546df145cac950547
Randsson <randssondouglas@gmail.com>
2023-12-10 21:13:15 -0300

    Passo 1 Crie modelo de Produto

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Student(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    validade = models.DateField()
+
+    def __str__(self):
+        return self.nome
